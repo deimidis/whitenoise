57b1177a5fce41a3d9d0350ad9398974ea0d475d
deimidis <deimidis@mozilla-hispano.org>
2013-01-08 21:43:06 -0300

    Deploy

---
+++ js/botonera.js
@@ -0,0 +1,1 @@
+define(["require","./jquery"],function(e){function r(){n("button.botones").each(function(){n(this).click(function(){var e=n(this).attr("class");alert(e)})})}var t="",n=e("./jquery");r()})
